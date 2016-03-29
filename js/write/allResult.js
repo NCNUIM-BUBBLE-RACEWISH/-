938738202bdd92a435a1ae145920fe026580691e
Jianggg777 <newstart0614@gmail.com>
2016-03-29 18:23:16 +0800

    查詢功能

--- js/write/allResult.js
+++ js/write/allResult.js
@@ -1,101 +1,86 @@
 $(document).ready(function() {
     var user = "資管女籃"
-        //取所有資料
-    getAll(user);
-});
-//取所有資料
-function getAll(user) {
+    //取所有資料
+    var data = getAll();
+    //塞入
+    pushIn(data);
+    //一進入頁面(mark所屬系球種)
+    search(user, data)
+    //點擊搜尋
     $(function() {
-        $.ajax({
-            url: "http://163.22.17.184:8080/bubble/Search_result/json",
-            data: {},
-            type: "GET",
-            //↓天辣辣辣辣這行太神了!!!!傳回值終於可以存惹感動
-            async: false,
-            success: function(data) {
-
-                for (i = 0; i < data.length; i++) {
-                    if ((data[i].星期 == "一") && (data[i].時間代碼 == "a")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#MonA").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#MonB > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#MonA").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-                    } else if ((data[i].星期 == "一") && (data[i].時間代碼 == "b")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#MonB").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#MonB > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#MonB").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-                    } else if ((data[i].星期 == "二") && (data[i].時間代碼 == "a")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#TueA").append("<p id = " + i + " > " + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#TueA > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#TueA").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-                    } else if ((data[i].星期 == "二") && (data[i].時間代碼 == "b")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#TueB").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#TueB > #" + i).css({ "background-color": "yellow" });
-
-                        } else {
-                            $("#TueB").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>")
-                        }
-                    } else if ((data[i].星期 == "三") && (data[i].時間代碼 == "a")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#WedA").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#WedA > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#WedA").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-                    } else if ((data[i].星期 == "三") && (data[i].時間代碼 == "b")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#WedB").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#WedB > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#WedB").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-                    } else if ((data[i].星期 == "四") && (data[i].時間代碼 == "a")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#ThuA").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#ThuA > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#ThuA").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-
-                    } else if ((data[i].星期 == "四") && (data[i].時間代碼 == "b")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#ThuB").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#ThuB > #" + i).css({ "background-color": "yellow" });
-
-                        } else {
-                            $("#ThuB").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-
-                        }
-
-                    } else if ((data[i].星期 == "五") && (data[i].時間代碼 == "a")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#FriA").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#FriA > #" + i).css({ "background-color": "yellow" });
-
-                        } else {
-                            $("#FriA").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-                    } else if ((data[i].星期 == "五") && (data[i].時間代碼 == "b")) {
-                        if ((data[i].使用者 + data[i].球種) == user) {
-                            $("#FriB").append("<p id=" + i + ">" + data[i].使用者 + data[i].球種 + data[i].場地代碼 + "</p>");
-                            $("#FriB > #" + i).css({ "background-color": "yellow" });
-                        } else {
-                            $("#FriB").append(data[i].使用者 + data[i].球種 + data[i].場地代碼 + "<br>");
-                        }
-
-                    }
-                }
-            }
+        $("#lookFor").click(function() {
+            //先將有mark的地方擦掉
+            remove();
+            var userSearch = $("#userSearch").val()
+            search(userSearch,data)
         });
     });
 
+});
+function getAll() {
+    var object = []
+    $.ajax({
+        url: "http://163.22.17.184:8080/bubble/Search_result/json",
+        data: {},
+        type: "GET",
+        //↓天辣辣辣辣這行太神了!!!!傳回值終於可以存惹感動
+        async: false,
+        success: function(data) {
+            for (i = 0; i < data.length; i++) {
+                var inner = {
+                        "使用者": data[i].使用者,
+                        "球種": data[i].球種,
+                        "星期": data[i].星期,
+                        "場地代碼": data[i].場地代碼,
+                        "時間代碼": data[i].時間代碼
+                    }
+                    //object[i]=inner
+                object.push(inner);
+            }
+            console.log(object)
+        }
+    });
+    return object
+}
+
+function pushIn(data) {
+    for (i = 0; i < data.length; i++) {
+        if ((data[i].星期 == "一") && (data[i].時間代碼 == "a")) {
+            $("#MonA").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "一") && (data[i].時間代碼 == "b")) {
+            $("#MonB").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "二") && (data[i].時間代碼 == "a")) {
+            $("#TueA").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "二") && (data[i].時間代碼 == "b")) {
+            $("#TueB").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>")
+        } else if ((data[i].星期 == "三") && (data[i].時間代碼 == "a")) {
+            $("#WedA").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "三") && (data[i].時間代碼 == "b")) {
+            $("#WedB").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "四") && (data[i].時間代碼 == "a")) {
+            $("#ThuA").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "四") && (data[i].時間代碼 == "b")) {
+            $("#ThuB").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "五") && (data[i].時間代碼 == "a")) {
+            $("#FriA").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        } else if ((data[i].星期 == "五") && (data[i].時間代碼 == "b")) {
+            $("#FriB").append("<p >"+data[i].場地代碼+"："+ data[i].使用者 + data[i].球種 + "</p>");
+        }
+    }
+}
+
+function search(user, data) {
+    //所有含有<P>的元素
+    var x = document.getElementsByTagName("p");
+    console.log(x)
+    for (i = 0; i < x.length; i++) {
+        var tmp = x[i].innerText
+        if (tmp.match(user) != null) {
+            x[i].innerHTML = '<font style="background:yellow;">' + tmp + '</font>';
+        }
+    }
+    return x;
+}
+function remove(){
+    $('font').removeAttr("style");
 }
