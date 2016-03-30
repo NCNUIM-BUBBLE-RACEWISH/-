ab8c964aaa40dca262e21ca91dbe4d82d20018fd
nancy010006 <denny512369874@gmail.com>
2016-03-30 13:52:42 +0800

    志願post包成json格式
    除了ballid其他部分已完成 等待帳號session

--- js/mytest.js
+++ js/mytest.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
     $("#volunteer").submit(function(event){
-        //var data = $("#volunteer").serializeArray();
-        //alert(JSON.stringify(data));
-        var jsonuserinfo = $('#volunteer').serializeObject();
-        alert(JSON.stringify(jsonuserinfo));
+        //console.log($(this).serializeArray());
+        var formData = $(this).serializeArray();
+        var dataObj ={};
+        var alldata =[];
+        for (var j = 0; j < 5; j++) {
+            for (var i = j*5; i < j*5+5; i++) {
+              var key = formData[i].name;
+              dataObj[key]=formData[i].value;
+            }
+            //console.log(dataObj);
+            alldata.push(dataObj);
+            dataObj = {};
+            console.log(JSON.stringify(alldata));
+        };
+        console.log(JSON.stringify(formData));
+        //console.log(JSON.stringify(alldata));
         $.ajax({
             url: 'http://163.22.17.184:8080/bubble/apply/json',
             contentType: "application/json",
-            data: JSON.stringify(jsonuserinfo),
+            data: JSON.stringify(dataObj),
             dataType: 'json',
             type: 'POST',
             success: function(data) {
@@ -19,12 +31,10 @@ $(document).ready(function(){
                 console.log(err);
             }
         });
-        console.log(JSON.stringify(jsonuserinfo));
+
         event.preventDefault();
     });
 });
-
-//將form裝成json
 $.fn.serializeObject = function()
 {
    var o = {};
@@ -41,3 +51,10 @@ $.fn.serializeObject = function()
    });
    return o;
 };
+/*
+function onClik(){
+        //var data = $("#test").serializeArray(); //自动将form表单封装成json
+        //alert(JSON.stringify(data));
+        var jsonuserinfo = $('#test').serializeObject();
+        alert(JSON.stringify(jsonuserinfo));
+}*/
