98049b5c4dcedcd195a64dcb7cfcbcdbdbe43a29
nancy010006 <denny512369874@gmail.com>
2016-04-26 20:28:48 +0800

    Revert "登入資料 session 志願"
    This reverts commit 1af569724193384909cd1788c06f2cf9a2e82d41.

--- js/write/login.js
+++ js/write/login.js
@@ -1,34 +1,24 @@
 $(document).ready(function(){
         $("#account").submit(function(event){
-                //將帳號密碼轉json
-                var kind=document.getElementById("kind").value;
-                var deptid=document.getElementById("deptid").value;
-                var passwd=document.getElementById("passwd").value;
+                var Balltype=document.getElementById("Balltype").value;
+                var YourDepartment=document.getElementById("YourDepartment").value;
+                var pwd=document.getElementById("pwd").value;
                 var data ={}
-                data.kind=kind;
-                data.deptid=deptid;
-                data.passwd=passwd;
+                data.Balltype=Balltype;
+                data.YourDepartment=YourDepartment;
+                data.pwd=pwd;
+                sessionStorage["data"]=JSON.stringify(data);
                 console.log(JSON.stringify(data));
+                window.location.replace("reroute.html");
                 $.ajax({
-                        url:'http://163.22.17.184:8080/bubble/Test/passwd',
+                        url:'https://github.com/NCNUIM-BUBBLE-RACEWISH/RaceWish-Server/blob/place/Test.java',
                         contentType:"application/json",
                         data:JSON.stringify(data),
                         dataType: 'json',
                         type: 'POST',
-                        success: function(result) {
+                        success: function(data) {
                                 console.log('woohoo!');
-                                console.log(result);
-                                // 成功時將帳號存入session
-                                if(result.statuscode==200){
-                                        sessionStorage["account"]=result.account;
-                                        document.location.href="reroute.html";
-                                }
-                                if(result.statuscode==401){
-                                        document.getElementById("status").innerHTML='帳號密碼錯誤!';
-                                }
-                                if(result.statuscode==500){
-                                        document.getElementById("status").innerHTML='伺服器問題 請稍後再試';
-                                }
+                                console.log(data);
                         },
                         error: function(err) {
                                 console.log('shit!');
@@ -38,7 +28,6 @@ $(document).ready(function(){
                 event.preventDefault();
         });
 });
-//測試用
 function printdata(){
-        console.log(sessionStorage["account"]);
+        console.log(sessionStorage["data"]);
 }
