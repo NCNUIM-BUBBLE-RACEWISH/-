2136a07f161ed54d337b4cbfee7deaffc1f4e8d9
nancy010006 <denny512369874@gmail.com>
2016-04-27 11:26:08 +0800

    加入身分認證js 分流 場地 填志願可用 首頁deptid value修正

---
+++ js/write/identity.js
@@ -0,0 +1,70 @@
+function showAccount(){
+        if(sessionStorage['account']=='010G')
+                document.getElementById('account').innerHTML='中文女籃';
+        else if(sessionStorage['account']=='010B')
+                document.getElementById('account').innerHTML='中文男籃'
+        else if(sessionStorage['account']=='020G')
+                document.getElementById('account').innerHTML='國比女籃'
+        else if(sessionStorage['account']=='020B')
+                document.getElementById('account').innerHTML='國比男籃'
+        else if(sessionStorage['account']=='030G')
+                document.getElementById('account').innerHTML='社工女籃'
+        else if(sessionStorage['account']=='030B')
+                document.getElementById('account').innerHTML='社工男籃'
+        else if(sessionStorage['account']=='040G')
+                document.getElementById('account').innerHTML='外文女籃'
+        else if(sessionStorage['account']=='040B')
+                document.getElementById('account').innerHTML='外文男籃'
+        else if(sessionStorage['account']=='050G')
+                document.getElementById('account').innerHTML='歷史女籃'
+        else if(sessionStorage['account']=='050B')
+                document.getElementById('account').innerHTML='歷史男籃'
+        else if(sessionStorage['account']=='060G')
+                document.getElementById('account').innerHTML='公行女籃'
+        else if(sessionStorage['account']=='060B')
+                document.getElementById('account').innerHTML='公行男籃'
+        else if(sessionStorage['account']=='070G')
+                document.getElementById('account').innerHTML='教政女籃'
+        else if(sessionStorage['account']=='070B')
+                document.getElementById('account').innerHTML='教政男籃'
+        else if(sessionStorage['account']=='080G')
+                document.getElementById('account').innerHTML='東南亞女籃'
+        else if(sessionStorage['account']=='080B')
+                document.getElementById('account').innerHTML='東南亞男籃'
+        else if(sessionStorage['account']=='110G')
+                document.getElementById('account').innerHTML='經濟女籃'
+        else if(sessionStorage['account']=='110B')
+                document.getElementById('account').innerHTML='經濟男籃'
+        else if(sessionStorage['account']=='120G')
+                document.getElementById('account').innerHTML='國企女籃'
+        else if(sessionStorage['account']=='120B')
+                document.getElementById('account').innerHTML='國企男籃'
+        else if(sessionStorage['account']=='130G')
+                document.getElementById('account').innerHTML='資管女籃'
+        else if(sessionStorage['account']=='130B')
+                document.getElementById('account').innerHTML='資管男籃'
+        else if(sessionStorage['account']=='140G')
+                document.getElementById('account').innerHTML='財金女籃'
+        else if(sessionStorage['account']=='140B')
+                document.getElementById('account').innerHTML='財金男籃'
+        else if(sessionStorage['account']=='210G')
+                document.getElementById('account').innerHTML='資工女籃'
+        else if(sessionStorage['account']=='210B')
+                document.getElementById('account').innerHTML='資工男籃'
+        else if(sessionStorage['account']=='220G')
+                document.getElementById('account').innerHTML='土木女籃'
+        else if(sessionStorage['account']=='220B')
+                document.getElementById('account').innerHTML='土木男籃'
+        else if(sessionStorage['account']=='230G')
+                document.getElementById('account').innerHTML='電機女籃'
+        else if(sessionStorage['account']=='230B')
+                document.getElementById('account').innerHTML='電機男籃'
+        else if(sessionStorage['account']=='240G')
+                document.getElementById('account').innerHTML='應化女籃'
+        else if(sessionStorage['account']=='240B')
+                document.getElementById('account').innerHTML='應化男籃'
+        else if(sessionStorage['account']=='420G')
+                document.getElementById('account').innerHTML='觀餐女籃'
+        else if(sessionStorage['account']=='420B')
+                document.getElementById('account').innerHTML='觀餐男籃'
+}
